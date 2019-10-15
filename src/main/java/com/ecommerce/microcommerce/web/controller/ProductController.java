9093c3193a58efb4412553b8a151444e1e3ba4b7
Romaric Tavenard <romaric.tavenard@etudiant.univ-lr.fr>
2019-10-15 14:39:39 +0200

    Partie 1 - Affichage de la marge

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -16,10 +16,10 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
 import java.net.URI;
-import java.util.List;
+import java.util.*;
 
 
-@Api( description="API pour es opérations CRUD sur les produits.")
+@Api(description = "API pour es opérations CRUD sur les produits.")
 
 @RestController
 public class ProductController {
@@ -56,12 +56,11 @@ public class ProductController {
 
         Product produit = productDao.findById(id);
 
-        if(produit==null) throw new ProduitIntrouvableException("Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
+        if (produit == null)
+            throw new ProduitIntrouvableException("Le produit avec l'id " + id + " est INTROUVABLE. Écran Bleu si je pouvais.");
 
         return produit;
     }
-
-
 
 
     //ajouter un produit
@@ -69,7 +68,7 @@ public class ProductController {
 
     public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
 
-        Product productAdded =  productDao.save(product);
+        Product productAdded = productDao.save(product);
 
         if (productAdded == null)
             return ResponseEntity.noContent().build();
@@ -83,13 +82,13 @@ public class ProductController {
         return ResponseEntity.created(location).build();
     }
 
-    @DeleteMapping (value = "/Produits/{id}")
+    @DeleteMapping(value = "/Produits/{id}")
     public void supprimerProduit(@PathVariable int id) {
 
         productDao.delete(id);
     }
 
-    @PutMapping (value = "/Produits")
+    @PutMapping(value = "/Produits")
     public void updateProduit(@RequestBody Product product) {
 
         productDao.save(product);
@@ -98,11 +97,27 @@ public class ProductController {
 
     //Pour les tests
     @GetMapping(value = "test/produits/{prix}")
-    public List<Product>  testeDeRequetes(@PathVariable int prix) {
+    public List<Product> testeDeRequetes(@PathVariable int prix) {
 
         return productDao.chercherUnProduitCher(400);
     }
 
+    //Calcule la marge de chaque produit
+    @GetMapping(value = "/AdminProduits")
+    Map<Product, Integer> calculerMargeProduit() {
+        Map<Product, Integer> produitEtMarge = new HashMap<>();
+        for (Product produit : productDao.findAll()
+        ) {
+            produitEtMarge.put(produit, produit.getPrix() - produit.getPrixAchat());
 
-
+        }
+        Map<Product, Integer> produitEtMargeTrie = new TreeMap<>(new Comparator<Product>() {
+            @Override
+            public int compare(Product p1, Product p2) {
+                return Integer.valueOf(p1.getId()).compareTo(p2.getId());
+            }
+        });
+        produitEtMargeTrie.putAll(produitEtMarge);
+        return produitEtMargeTrie;
+    }
 }
